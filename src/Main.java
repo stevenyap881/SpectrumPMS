ed398aec632eea626cd0eb24531fa343ac379886
stevenyap881 <stevenyap881@gmail.com>
2021-04-24 22:11:37 +0800

    PMS4

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Testing from Spectrum PMS!");
         System.out.println("Testing from Spectrum PMS2!");
         System.out.println("Testing from Spectrum PMS3!");
+        System.out.println("Testing from Spectrum PMS4!");
     }
 }
