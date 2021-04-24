3c0317bf064f507570e63b081f92a2a402f9ab17
stevenyap881 <stevenyap881@gmail.com>
2021-04-24 22:08:18 +0800

    PMS3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing from Spectrum PMS!");
         System.out.println("Testing from Spectrum PMS2!");
+        System.out.println("Testing from Spectrum PMS3!");
     }
 }
