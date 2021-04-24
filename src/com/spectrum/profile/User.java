534c6e74dc8dca3b192f6b3d6ba5ecb5d1fc3960
stevenyap881 <stevenyap881@gmail.com>
2021-04-24 22:17:15 +0800

    PMS 5

---
+++ src/com/spectrum/profile/User.java
@@ -0,0 +1,78 @@
+package com.spectrum.profile;
+
+public class User extends UserProfile {
+
+    String str_firstName;
+    String str_middleName;
+    String str_lastName;
+
+    String str_address1;
+    String str_address2;
+    String str_address3;
+    String str_address4;
+    String str_address5;
+
+    public String getStr_firstName() {
+        return str_firstName;
+    }
+
+    public void setStr_firstName(String str_firstName) {
+        this.str_firstName = str_firstName;
+    }
+
+    public String getStr_middleName() {
+        return str_middleName;
+    }
+
+    public void setStr_middleName(String str_middleName) {
+        this.str_middleName = str_middleName;
+    }
+
+    public String getStr_lastName() {
+        return str_lastName;
+    }
+
+    public void setStr_lastName(String str_lastName) {
+        this.str_lastName = str_lastName;
+    }
+
+    public String getStr_address1() {
+        return str_address1;
+    }
+
+    public void setStr_address1(String str_address1) {
+        this.str_address1 = str_address1;
+    }
+
+    public String getStr_address2() {
+        return str_address2;
+    }
+
+    public void setStr_address2(String str_address2) {
+        this.str_address2 = str_address2;
+    }
+
+    public String getStr_address3() {
+        return str_address3;
+    }
+
+    public void setStr_address3(String str_address3) {
+        this.str_address3 = str_address3;
+    }
+
+    public String getStr_address4() {
+        return str_address4;
+    }
+
+    public void setStr_address4(String str_address4) {
+        this.str_address4 = str_address4;
+    }
+
+    public String getStr_address5() {
+        return str_address5;
+    }
+
+    public void setStr_address5(String str_address5) {
+        this.str_address5 = str_address5;
+    }
+}
---
+++ src/com/spectrum/profile/UserProfile.java
@@ -0,0 +1,16 @@
+package com.spectrum.profile;
+
+public abstract class UserProfile {
+
+    String str_firstName;
+    String str_middleName;
+    String str_lastName;
+
+    String str_address1;
+    String str_address2;
+    String str_address3;
+    String str_address4;
+    String str_address5;
+
+
+}
