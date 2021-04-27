d55c693a9e5aa48e77a79c76af6b9e10ec6fcfd2
stevenyap881 <stevenyap881@gmail.com>
2021-04-27 14:44:56 +0800

    PMS 8.0 from ubuntu
    Signed-off-by: steven <stevenyap881@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Testing from Spectrum PMS5!");
         System.out.println("Testing from Spectrum PMS6.0!");
         System.out.println("Testing from Spectrum PMS7.0!");
+        System.out.println("Testing from Spectrum PMS8.0!");
     }
 }
